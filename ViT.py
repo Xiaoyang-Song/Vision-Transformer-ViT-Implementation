d46ac054a5892f74e570069bf5cb221ca956f3ae
xysong <xysong@umich.edu>
2021-12-08 03:25:16 -0500

    ViT Code Skeleton Setup

--- ViT.py
+++ ViT.py
@@ -9,3 +9,12 @@ class Encoder(nn.Module):
 
     def forward(self):
         pass
+
+
+class PatchEmbedding(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
